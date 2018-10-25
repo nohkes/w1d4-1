3c056cc35059f09e0e14e89b4123b72739f1d7aa
nohkes <41023611+nohkes@users.noreply.github.com>
2018-10-25 12:27:37 -0400

    Update map.js
    syntax mistakes

--- map.js
+++ map.js
@@ -3,11 +3,11 @@ var input = [
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(var result = input.map(newobj=>{
+var result = input.map(newobj=>{
 
     return Math.sqrt(Math.pow(newobj.x,2)+Math.pow(newobj.y,2)); 
 
-}));
+});
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
