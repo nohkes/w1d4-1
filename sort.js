d382ca87f63544df5e2b22245e772dcfe64659a3
Rawle Sekhon <amandeepsekhon404@gmail.com>
2018-10-25 15:45:23 +0000

    using the sort method

---
+++ sort.js
@@ -0,0 +1,23 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+var newList = students.sort(function(a, b){
+	return b.age - a.age
+});
+
+newList.sort(function(a, b){
+	var nameA = a.name;
+	var nameB = b.name;
+	if ( nameA < nameB){
+		return - 1
+	}
+	if (nameA > nameB){
+		return 1
+	}
+	return 0
+})
+console.log(newList)
